1e4ddea15e61c4eeb4199f21edd5a2c5b2baf7db
Sergey <a_s_v86@mail.ru>
2022-08-02 23:03:59 +0300

    Styles and Function

--- src/index.js
+++ src/index.js
@@ -1,17 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import "./index.css";
+
+const Name = 'Sergey'
+const Age = '28'
+
+const FunctionalComponent = (props) => {
+  return <div>
+    props.Name = {Name}
+    props.Age = {Age}
+  </div>
+}
+
+const Parent = () => {
+
+  return (
+    <div className="test">
+      <FunctionalComponent
+        Age={Age} Name={Name} />
+    </div>
+  )
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Parent />
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
